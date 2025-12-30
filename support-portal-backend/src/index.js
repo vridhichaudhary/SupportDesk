a01bc10ca29996f196f85785032e473f5a5e110a
vridhichaudhary <vridhichaudhary05@gmail.com>
2025-12-30 15:27:49 +0530

    cors

--- support-portal-backend/src/index.js
+++ support-portal-backend/src/index.js
@@ -26,7 +26,9 @@ const allowedOrigins = [
 
 app.use(cors({
     origin: allowedOrigins,
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(bodyParser.json());
